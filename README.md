14285d72710023bd8b735da2a33e56ccffb23415
jeanriffel <jeanriffel1@gmail.com>
2022-03-27 14:43:15 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Redis-cache
+
+This is a repo for studying Redis.
+
+Here I want to share simple examples which I have been using for studying.
+
+### REDIS
+
+Let's use the docker container of Redis to test how use it into a NodeJS app.
+
+Access Redis Docker for installing<br> 
+- [Redis Docker](https://hub.docker.com/_/redis)  
+
+
+Create a new network using the command below
+- ```docker network create my-own-network```
+
+Run the container using the command below
+- ```docker run --name myredis --network my-own-network -p 6379:6379 -d redis```
+
+- use the command ```docker ps``` for cheking if your docker container is running well.
+
+
+### App NodeJS
+
+- type ```nvm use``` to use the right version of Node.
+
+- ```npm install``` for installing all dependencies.
+
+
---
+++ server.js
@@ -0,0 +1,38 @@
+const redis = require('redis');
+const redisClient = redis.createClient();
+
+const connectClient = ()=>{    
+    return new Promise( (resolve, reject)=>{
+        redisClient.connect()
+        .then( ()=>{
+            resolve(true);
+        })
+        .catch((error)=>{
+            reject(error)
+        });
+    });
+}
+
+const main = async ()=>{
+    try {
+        const connected = await connectClient();
+        console.log(`Is connected ${ connected} `);
+        const key = 'userName'
+        const name = 'Mary';
+
+        if(connected){
+            const existKey = await redisClient.exists(key);
+            if (!existKey){                
+                await redisClient.set(key, name);
+                console.log(`setting key [ ${key} ] with value [ ${name} ] `);
+            }else{
+                const valueKey = await redisClient.get('name1')
+                console.log(`the key [ ${key} ] already exists in cache with value [ ${valueKey} ] `);
+            }   
+        }
+    }catch(error){
+        console.error(error);
+    }
+}
+
+main();
